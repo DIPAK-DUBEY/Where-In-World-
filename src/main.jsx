7b7030cab771710c2059674d196884f18d53f3bb
Dipak Dubey <dipakdubey767@gmail.com>
2026-02-26 00:16:36 +0530

    Added one more route for accessing country values my clicking on the region countries

--- src/main.jsx
+++ src/main.jsx
@@ -7,34 +7,36 @@ import BorderCountry from './Components/BorderCountry.jsx'
 import ByRegion from './Components/ByRegion.jsx'
 import ThemeSync from './Components/ThemeSync.jsx'
 import SmoothScrolling from "./Components/Scrolling";
+import RegionSingleCountry from './Components/RegionSingleCountry.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <App />
-      </>
-
-    )
-  },
-  {
-    path: '/Country/:id',
-    element: <SingleCountry />
-  },
-  {
-    path: '/Border/:code',
-    element: <BorderCountry />
-  }, {
-    path: 'Region/:region',
-    element: <ByRegion />
-  }
-]);
-
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />
+    },
+    {
+      path: '/Country/:id',
+      element: <SingleCountry />
+    },
+    {
+      path: '/Border/:code',
+      element: <BorderCountry />
+    },
+    {
+      path: '/Region/:region',
+      element: <ByRegion />,
+    },
+    {
+      path: '/Region/:region/Country/:id',
+      element: <SingleCountry />   
+    }
+  ]
+)
 createRoot(document.getElementById('root')).render(
   <>
     <ThemeSync />
-    <SmoothScrolling/>
+    <SmoothScrolling />
     <RouterProvider router={router} />
   </>
 
